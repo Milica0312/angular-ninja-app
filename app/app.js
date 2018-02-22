91dfbd2ecbb2fcfb8d9dea5ef60e18ad71259188
Milica0312 <milicapavlovic0312@gmail.com>
2018-02-22 21:35:02 +0100

    Location service

--- app/app.js
+++ app/app.js
@@ -7,7 +7,12 @@ myNinjaApp.config(['$routeProvider', function($routeProvider){
         controller: 'NinjaController'
     })
     .when('/contact', {
-        templateUrl: 'views/contact.html'
+        templateUrl: 'views/contact.html',
+        controller: 'ContactController'
+    })
+    .when('/contact-success', {
+        templateUrl: 'views/contact-success.html',
+        controller: 'ContactController'
     })
     .when('/directory', {
         templateUrl: 'views/directory.html',
@@ -97,4 +102,10 @@ $scope.addNinja = function(){
         $scope.ninjas = response.data;
     });
 
+}]);
+
+myNinjaApp.controller('ContactController', ['$scope', '$location', function($scope, $location){
+  $scope.sendMessage = function(){
+    $location.path('/contact-success');
+  };
 }]);
